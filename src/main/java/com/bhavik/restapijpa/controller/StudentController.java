7d07bea6dd3c104c53f465f8554f4cd33cfb2b73
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-21 10:21:58 +0530

    Spring REST API: POST Req

--- src/main/java/com/bhavik/restapijpa/controller/StudentController.java
+++ src/main/java/com/bhavik/restapijpa/controller/StudentController.java
@@ -2,27 +2,33 @@ package com.bhavik.restapijpa.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bhavik.restapijpa.model.Student;
+import com.bhavik.restapijpa.service.StudentRepo;
 
 @RestController
 public class StudentController {
-	
-	
+
+	@Autowired
+	private StudentRepo repository;
+
 	@GetMapping("/studentsAPI")
-	public List<Student> getAllStudents(){
+	public List<Student> getAllStudents() {
 		return null;
 	}
-	
+
 	@PostMapping("/studentsAPI")
-	public ResponseEntity<Object> addNewStudent(){
-		
-		return new ResponseEntity("Student Added",HttpStatus.OK);
+	public ResponseEntity<Object> addNewStudent(@RequestBody Student student) {
+		Student s1= repository.save(student);
+		System.out.println(s1);
+		return new ResponseEntity("Student Added"+ s1, HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/bhavik/restapijpa/model/Student.java
+++ src/main/java/com/bhavik/restapijpa/model/Student.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Student {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
 	String name;
 	int age;
