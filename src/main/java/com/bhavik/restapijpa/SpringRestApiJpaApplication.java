6fea67847e4553bea325f8aaa51749565bcff4be
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-21 09:40:42 +0530

    initial commit

---
+++ src/main/java/com/bhavik/restapijpa/SpringRestApiJpaApplication.java
@@ -0,0 +1,13 @@
+package com.bhavik.restapijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavik/restapijpa/SpringRestApiJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavik.restapijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
