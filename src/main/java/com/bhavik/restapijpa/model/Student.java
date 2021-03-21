3c4a168fdd98906ed53bbd32561efbd0370942c1
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-21 10:00:29 +0530

    improve the Student Class

--- src/main/java/com/bhavik/restapijpa/model/Student.java
+++ src/main/java/com/bhavik/restapijpa/model/Student.java
@@ -1,6 +1,5 @@
 package com.bhavik.restapijpa.model;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,10 +11,59 @@ import javax.persistence.Table;
 public class Student {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	int id;
 	String name;
 	int age;
 	String department;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public Student() {
+
+	}
+
+	public Student(int id, String name, int age, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + ", department=" + department + "]";
+	}
 
 }
